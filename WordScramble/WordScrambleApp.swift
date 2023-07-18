6f06a70c7377a92078e11a70ed2c4c5d1afa201d
Keenan Chiasson <keenanrc@hotmail.com>
2023-07-18 12:49:19 -0500

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by user932953 on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
